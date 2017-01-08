a72b8225f5051aa7fd84adbcf8c42493b8dea7c0
Ralph Morelli <ram8647@gmail.com>
2017-01-08 12:34:57 -0500

    Additional style changes.

--- cspquiz/quiz_code.js
+++ cspquiz/quiz_code.js
@@ -188,9 +188,8 @@
     var feedback = document.getElementById("feedback");
     feedback.style.visibility="hidden";
     var question = document.getElementById('question');
-     //  question.innerHTML = curr_question['question'];
-     //  run through convertAPML(input)
-    question.innerHTML = convertAPML(curr_question['question']);
+    var question_txt = "<b>"+curr_question['heading']+".</b>&nbsp;" + curr_question['question'];
+    question.innerHTML = convertAPML(question_txt); // convertAPML
   }
 
   /*
